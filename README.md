4954db8e36be022f5dbdf262f8dd28cb9a15f624
noryev <logan.lentz1@gmail.com>
2022-08-13 23:32:13 -0500

    cant sleep

---
+++ README.md
@@ -0,0 +1,12 @@
+# Getting Started with adding IPFS into a React UI (with MaterialUI)
+
+Start by importing web3.storage tools and try some of those example functions-
+
+Put your api key inside the env file.
+
+## Getting Started
+
+npm install
+
+npm start
+
---
+++ src/styles.js
@@ -0,0 +1,34 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+// you have this theme object available from MaterialUI- learn more!
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6)
+        
+    },
+    icon: {
+        marginRight: '20px' ,
+    },
+    buttons: {
+        marginTop: '40px'
+    },
+    cardGrid: {
+        padding: '20px 0'
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    CardMedia: {
+        paddingTop: '56.25%' // 16:9 aspect ratio
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+}));
+
+export default useStyles;
